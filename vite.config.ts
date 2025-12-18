9d4157e448b50eb6363a28a4022cd503c9008286
leolimacr <leolimacr@hotmail.com>
2025-12-17 21:23:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // <--- LINHA ADICIONADA: Isso corrige o erro de tela branca na Vercel
       server: {
         port: 3000,
         host: '0.0.0.0',
